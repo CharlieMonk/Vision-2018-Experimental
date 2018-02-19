b989a032a1475f6647290264d9c75b6f2dd7600e
Charlie Monk <CharlieMonk@users.noreply.github.com>
2018-02-19 10:25:11 -0500

    cv2.matchTemplate only returns the upper left point, so this code flips the image over the y-axis so it can get the upper right point as well.

--- find_scale.py
+++ find_scale.py
@@ -16,16 +16,16 @@ query = cv2.imread("/Users/cbmonk/Downloads/query2.png",0)
 w, h = query.shape[::-1]
 # All the 6 algorithms for comparison in a list
 time0 = time.time()
-alg = eval("cv2.TM_CCOEFF")
 # Look for matches
-res = cv2.matchTemplate(img, query, alg)
-print("Res: ", res)
-top_left = cv2.minMaxLoc(res)[3]
+top_left = searchImage(img, query)
+flip_img = cv2.flip(img, 1)
+top_right = searchImage(flip_img, query)
 #print(min_val, max_val, min_loc, top_left)
-bottom_right = (top_left[0] + w, top_left[1] + h)
+bottom_right = (img.shape[0] - top_right[0], top_left[1] + h)
 cv2.rectangle(bgr_img, top_left, bottom_right, (255,0,0), 2)
-#cv2.circle(bgr_img, min_loc, 2, (0,255,0), 7)
-cv2.circle(bgr_img, top_left, 2, (0,255,0), 7)
+cv2.circle(flip_img, (img.shape[0]*2-top_left[0], top_left[1]), 2, (0,255,0), 7)
+cv2.circle(flip_img, top_right, 2, (0,255,0), 7)
+cv2.imshow("Flipped", flip_img)
 print("Time: " + str(time.time()-time0))
 cv2.imshow("Detected Points", bgr_img)
 cv2.waitKey(0)
